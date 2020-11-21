843e605ad5bed00829b31f6032d158330930cb8e
Wesley <wspencer3295@yahoo.com>
2020-11-21 01:37:58 -0700

    commit tree d917afdde9b316330d7e2e4a907c745063d7f05d

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
-def starts_with_a_vowel?(word)
 
-end
+
+def starts_with_a_vowel?(word)
+ word.match?(/\b[aeiouAEIOU]\w+/)
+   
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match?(/^\b[A-Z].+[\.!?]$/)
 end
 
 def valid_phone_number?(phone)
+#  phone.match?(/\b\d*{0,9}[ ]?|\(\d{3}\)[ -]?\d{3}[ -]?\d{4}/)
+#  (/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+phone.match?(/([0-9] *){10}|(\([0-9]{3}\)([0-9]{3}-[0-9]{4})|[0-9]{7})\b/)
 
 end
